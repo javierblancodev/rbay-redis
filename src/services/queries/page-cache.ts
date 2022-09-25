4b00d5ca26719b0785c7ccc2dbd04d7b8eba37a9
Javier <javierblancoit@gmail.com>
2022-09-25 07:51:34 +0100

    add functionality to get and set static pages into redis db when hitting the endpoints: about, privacy, signin, signup

--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,3 +1,21 @@
-export const getCachedPage = (route: string) => {};
+import { client } from "$services/redis";
 
-export const setCachedPage = (route: string, page: string) => {};
+const cacheRoutes = [
+    '/about', 'privacy', 'auth/singin', '/auth/singup'
+]
+
+export const getCachedPage = (route: string) => {
+    if(cacheRoutes.includes(route)) {
+        return client.get('pagecache#' + route);
+    }
+
+    return null;
+};
+
+export const setCachedPage = (route: string, page: string) => {
+    if(cacheRoutes.includes(route)) {
+        client.set('pagecache#' + route, page, {
+            EX: 2
+        })
+    }
+};
